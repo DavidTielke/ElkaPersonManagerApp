fccc1f1cd0f4b8a0a64e429c782de93faa80f0e4
David Tielke <david.tielke@hotmail.de>
2023-03-21 09:27:31 +0100

    Dreckige Implementierung

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace PersonManagerApp.ConsoleClient;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var persons = File
+            .ReadAllLines("data.csv")
+            .Select(l => l.Split(","))
+            .Select(p => new Person
+            {
+                Id = int.Parse(p[0]),
+                Name = p[1],
+                Age = int.Parse(p[2])
+            })
+            .ToList();
+
+        var adults = persons.Where(p => p.Age >= 18).ToList();
+        var children = persons.Where(p => p.Age < 18).ToList();
+
+        Console.WriteLine($"## Erwachsene ({adults.Count}) ##");
+        adults.ToList().ForEach(p => Console.WriteLine(p.Name));
+
+        Console.WriteLine($"## Kinder ({children.Count}) ##");
+        children.ToList().ForEach(c => Console.WriteLine(c.Name));
+    }
+}
